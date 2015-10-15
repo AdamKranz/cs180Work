d04157d7da28db3cc14db3c4354f52630e8b688e
AdamKranz <akranz@purdue.edu>
2015-10-14 21:53:05 -0400

    Added Racecar.java

---
+++ src/Racecar.java
@@ -0,0 +1,17 @@
+/**
+ * Created by akranz on 10/14/15.
+ */
+public class Racecar extends Car {
+    private int speed;
+    public Racecar(String make, int speed) {
+        super(make);
+        this.speed = speed;
+    }
+    public void race() {
+        super.drive();
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
