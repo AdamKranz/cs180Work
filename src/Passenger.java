57a116d1836cc6fb9ad37630adabb63bf8888742
AdamKranz <akranz@purdue.edu>
2015-10-19 01:47:28 -0400

    Updated Passenger and added Route

--- src/Passenger.java
+++ src/Passenger.java
@@ -3,5 +3,29 @@ public class Passenger {
 	public static final String CANCELED = "Canceled";
 	public static final String WAITLIST = "Waitlist";
 	public static final String CONFIRMED = "Confirmed";
+	private String bookingStatus;
+	private String name;
+	private Route route;
+
+	public Passenger(String name, Route route) {
+		this.name = name;
+		this.route = route;
+		bookingStatus = WAITLIST;
+	}
+	public String getPassengerName() {
+		return name;
+	}
+	public Route getRoute() {
+		return route;
+	}
+	public String getBookingStatus() {
+		return bookingStatus;
+	}
+	public void confirm() {
+		bookingStatus = CONFIRMED;
+	}
+	public void cancel() {
+		bookingStatus = CANCELED;
+	}
 
 }
---
+++ src/Route.java
@@ -0,0 +1,14 @@
+/**
+ * Created by akranz on 10/19/15.
+ */
+public class Route {
+    private String from;
+    private String to;
+    public Route(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+    public boolean equals(Object obj) {
+        return false;
+    }
+}
