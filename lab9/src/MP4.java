493b247f6f9a257c434b2a3b4d843ba3670cb84e
AdamKranz <akranz@purdue.edu>
2015-10-20 21:07:00 -0400

    Finished MP4 Class

--- lab9/src/MP4.java
+++ lab9/src/MP4.java
@@ -1,5 +1,23 @@
+import java.util.UUID;
+
 /**
  * Created by akranz on 10/20/15.
  */
-public class MP4 {
+public class MP4 extends VideoProduct implements Downloadable {
+    public MP4(String productName, VideoType type) {
+        super(productName, type);
+    }
+    @Override
+    public double getPrice() {
+        if (this.getType() == VideoType.MOVIE)
+            return 4.99;
+        if (this.getType() == VideoType.TVSERIES)
+            return 19.99;
+
+        return 0.0;
+    }
+    @Override
+    public String generateDownloadCode() {
+        return UUID.randomUUID().toString();
+    }
 }
