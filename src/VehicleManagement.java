7f2779f7193e3cb5df3a16433a84a9a4e79ee95c
AdamKranz <akranz@purdue.edu>
2015-10-23 02:58:25 -0400

    Did 9/11

--- src/VehicleManagement.java
+++ src/VehicleManagement.java
@@ -1,82 +1,30 @@
-import java.util.ArrayList;
+import java.*;
+import java.util.Arrays;
 
 /**
  * Created by akranz on 10/20/15.
  */
 public class VehicleManagement {
-    private int busMax;
-    private int planeMax;
-    private ArrayList<Vehicle> vehicles;
-
+    private int bus;
+    private int plane;
+    private Vehicle[] vehicles;
     public VehicleManagement(int bus, int plane) {
-        busMax = bus;
-        planeMax = plane;
-        vehicles = new ArrayList<>();
+        this.bus = bus;
+        this.plane = plane;
+        vehicles = new Vehicle[0];
     }
-
     public boolean createRoute(Route route, int capacity) {
-        if (route.equals(null))
-            return false;
-        for (Vehicle v : vehicles) {
+        for (Vehicle v : vehicles)
             if (v.getRoute().equals(route))
                 return false;
-        }
-        Bus b = new Bus(route, capacity);
-        if (vehicles.size()>0) {
-            for (int i = 0; i < vehicles.size(); i++) {
-                return !vehicles.get(i).getRoute().equals(b.getRoute());
-            }
-        }
+        Vehicle[] v = new Vehicle[vehicles.length+1];
+        System.arraycopy(vehicles, 0, v, 0, vehicles.length);
+        vehicles = v;
+        Bus newBus = new Bus(route, capacity);
+        vehicles[vehicles.length-1] = newBus;
         return true;
     }
     public boolean addPassengerToVehicle(Passenger person) {
-        for (Vehicle v : vehicles) {
-            if (person.getRoute().equals(v.getRoute())) {
-                   if (v.addPassenger(person)) {
-                       if (v.getPassengers().length>=v.getCapacity()*2) {
-                           boolean hasAirplane = false;
-                           for (Vehicle s : vehicles) {
-                               if (s instanceof Airplane)
-                                   hasAirplane = true;
-                           }
-                           if (hasAirplane) {
-                               v.upgrade(v.getCapacity()*3);
-                           }
-                       }
-                   }
-            }
-        }
-        return false;
-    }
-    public Vehicle[] getVehicles() {
-        Vehicle[] v = new Vehicle[vehicles.size()];
-        return vehicles.toArray(v);
-    }
-    public int getCount() {
-        return vehicles.size();
-    }
-    public int lookupVehicle(Route route) {
-        for (int i = 0; i < vehicles.size(); i++) {
-            if (vehicles.get(i).getRoute().equals(route))
-                return i;
-        }
-        return -1;
-    }
-    public int getAvailableBus() {
-        int busCount = 0;
-        for (Vehicle v : vehicles) {
-            if (v instanceof Bus)
-                busCount++;
-        }
-        return busCount;
-    }
-    public int getAvailablePlane() {
-        int planeCount = 0;
-        for (Vehicle v : vehicles) {
-            if (v instanceof Airplane)
-                planeCount++;
-        }
-        return planeCount;
-    }
 
+    }
 }
